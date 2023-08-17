711097a253512d31ce63aac68b1fd4e940019e30
nhathoa2002 <142466074+nhathoa2002@users.noreply.github.com>
2023-08-17 19:55:33 +0700

    Add files via upload

--- promises.d.ts
+++ promises.d.ts
@@ -1,42 +1,68 @@
-declare module 'stream/promises' {
-    import { FinishedOptions, PipelineSource, PipelineTransform, PipelineDestination, PipelinePromise, PipelineOptions } from 'node:stream';
-    function finished(stream: NodeJS.ReadableStream | NodeJS.WritableStream | NodeJS.ReadWriteStream, options?: FinishedOptions): Promise<void>;
-    function pipeline<A extends PipelineSource<any>, B extends PipelineDestination<A, any>>(source: A, destination: B, options?: PipelineOptions): PipelinePromise<B>;
-    function pipeline<A extends PipelineSource<any>, T1 extends PipelineTransform<A, any>, B extends PipelineDestination<T1, any>>(
-        source: A,
-        transform1: T1,
-        destination: B,
-        options?: PipelineOptions
-    ): PipelinePromise<B>;
-    function pipeline<A extends PipelineSource<any>, T1 extends PipelineTransform<A, any>, T2 extends PipelineTransform<T1, any>, B extends PipelineDestination<T2, any>>(
-        source: A,
-        transform1: T1,
-        transform2: T2,
-        destination: B,
-        options?: PipelineOptions
-    ): PipelinePromise<B>;
-    function pipeline<
-        A extends PipelineSource<any>,
-        T1 extends PipelineTransform<A, any>,
-        T2 extends PipelineTransform<T1, any>,
-        T3 extends PipelineTransform<T2, any>,
-        B extends PipelineDestination<T3, any>
-    >(source: A, transform1: T1, transform2: T2, transform3: T3, destination: B, options?: PipelineOptions): PipelinePromise<B>;
-    function pipeline<
-        A extends PipelineSource<any>,
-        T1 extends PipelineTransform<A, any>,
-        T2 extends PipelineTransform<T1, any>,
-        T3 extends PipelineTransform<T2, any>,
-        T4 extends PipelineTransform<T3, any>,
-        B extends PipelineDestination<T4, any>
-    >(source: A, transform1: T1, transform2: T2, transform3: T3, transform4: T4, destination: B, options?: PipelineOptions): PipelinePromise<B>;
-    function pipeline(streams: ReadonlyArray<NodeJS.ReadableStream | NodeJS.WritableStream | NodeJS.ReadWriteStream>, options?: PipelineOptions): Promise<void>;
-    function pipeline(
-        stream1: NodeJS.ReadableStream,
-        stream2: NodeJS.ReadWriteStream | NodeJS.WritableStream,
-        ...streams: Array<NodeJS.ReadWriteStream | NodeJS.WritableStream | PipelineOptions>
-    ): Promise<void>;
+/**
+ * The `timers/promises` API provides an alternative set of timer functions
+ * that return `Promise` objects. The API is accessible via`require('timers/promises')`.
+ *
+ * ```js
+ * import {
+ *   setTimeout,
+ *   setImmediate,
+ *   setInterval,
+ * } from 'timers/promises';
+ * ```
+ * @since v15.0.0
+ */
+declare module 'timers/promises' {
+    import { TimerOptions } from 'node:timers';
+    /**
+     * ```js
+     * import {
+     *   setTimeout,
+     * } from 'timers/promises';
+     *
+     * const res = await setTimeout(100, 'result');
+     *
+     * console.log(res);  // Prints 'result'
+     * ```
+     * @since v15.0.0
+     * @param [delay=1] The number of milliseconds to wait before fulfilling the promise.
+     * @param value A value with which the promise is fulfilled.
+     */
+    function setTimeout<T = void>(delay?: number, value?: T, options?: TimerOptions): Promise<T>;
+    /**
+     * ```js
+     * import {
+     *   setImmediate,
+     * } from 'timers/promises';
+     *
+     * const res = await setImmediate('result');
+     *
+     * console.log(res);  // Prints 'result'
+     * ```
+     * @since v15.0.0
+     * @param value A value with which the promise is fulfilled.
+     */
+    function setImmediate<T = void>(value?: T, options?: TimerOptions): Promise<T>;
+    /**
+     * Returns an async iterator that generates values in an interval of `delay` ms.
+     *
+     * ```js
+     * import {
+     *   setInterval,
+     * } from 'timers/promises';
+     *
+     * const interval = 100;
+     * for await (const startTime of setInterval(interval, Date.now())) {
+     *   const now = Date.now();
+     *   console.log(now);
+     *   if ((now - startTime) > 1000)
+     *     break;
+     * }
+     * console.log(Date.now());
+     * ```
+     * @since v15.9.0
+     */
+    function setInterval<T = void>(delay?: number, value?: T, options?: TimerOptions): AsyncIterable<T>;
 }
-declare module 'node:stream/promises' {
-    export * from 'stream/promises';
+declare module 'node:timers/promises' {
+    export * from 'timers/promises';
 }
